247802bda313f76d1bad3ecff33ab3f0ac2bbe73
Radele Rose <radele.rose@gmail.com>
2020-08-28 05:45:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,25 @@ def display_board
  puts "-----------"
  puts "   |   |   "
 end
+
+def valid_move?(board, index)
+  if index.between?(1,9)
+    if !position_taken?(board, index)
+      true
+    end
+  end
+
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+
+ if (board[index] == " " || board[index] == "" || board[index] == nil)
+   taken = false
+else
+  taken = true
+end
+
+(board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+   false : true
+end
